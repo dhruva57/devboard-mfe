6c6f2320343005b10d94e753a1037f1d9bff57f5
dhruva tripathi <tripathi.dhruva05@gmail.com>
2026-04-05 00:03:30 +0530

    feat: cleanup env.example; move remote urls to env

--- common/vite.config.ts
+++ common/vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import tailwindcss from "@tailwindcss/vite";
 import federation from "@originjs/vite-plugin-federation";
 
 export default defineConfig({
   plugins: [
     react(),
-    // tailwindcss(),
     federation({
       name: "common_remote",
       filename: "remoteEntry.js",
--- mfe-tasks/vite.config.ts
+++ mfe-tasks/vite.config.ts
@@ -1,34 +1,38 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import federation from "@originjs/vite-plugin-federation";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    federation({
-      name: "mfeTasks",
-      filename: "remoteEntry.js",
-      exposes: {
-        "./App": "./src/App",
-      },
-      remotes: {
-        common_remote: "http://localhost:5002/assets/remoteEntry.js",
-      },
-      shared: ["react", "react-dom"],
-    }),
-  ],
-  server: {
-    port: 5001,
-  },
-  preview: {
-    port: 5001,
-  },
-  build: {
-    target: "esnext",
-    modulePreload: false,
-    minify: false,
-    cssCodeSplit: false,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      federation({
+        name: "mfeTasks",
+        filename: "remoteEntry.js",
+        exposes: {
+          "./App": "./src/App",
+        },
+        remotes: {
+          common_remote: env.VITE_COMMON_REMOTE,
+        },
+        shared: ["react", "react-dom"],
+      }),
+    ],
+    server: {
+      port: 5001,
+    },
+    preview: {
+      port: 5001,
+    },
+    build: {
+      target: "esnext",
+      modulePreload: false,
+      minify: false,
+      cssCodeSplit: false,
+    },
+  };
 });
